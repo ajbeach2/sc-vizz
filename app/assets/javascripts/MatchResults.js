186b8fc763d4b67999112f8e967e64c18b037800
Alexander Beach <ajbeach2@gmail.com>
2014-04-26 16:54:08 -0500

    refactor RenderItemMatcher and MatchResults into amd modules

---
+++ app/assets/javascripts/MatchResults.js
@@ -0,0 +1,10 @@
+define(
+    "MatchResults",
+    function(MatchResults) {
+        function MatchResults(){
+            this.unmatchedLeft = [];
+            this.unmatchedRight = [];
+        }
+        return MatchResults;
+    }
+);
